6a91a963e43653e91d1ed20daaf720ffa26f8ae6
Pratik Pattanaik <pratik.a.pattanaik@accenture.com>
2025-01-25 12:15:25 +0530

    solved bubble sort problem in python

--- Python Practise Questions/bubbleSort.py
+++ Python Practise Questions/bubbleSort.py
@@ -3,5 +3,18 @@ Sorts the given array in ascending order using the bubble sort algorithm.
 
 """
 
-def numBubblesort(arr):
-    pass
+def numBubblesort(arr:list) -> list:
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# Example usage
+data = [64, 34, 25, 12, 22, 11, 90]
+print("Unsorted Array:", data)
+
+sorted_data = numBubblesort(data)
+print("Sorted Array:", sorted_data)
