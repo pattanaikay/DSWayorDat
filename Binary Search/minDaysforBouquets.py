a5056ac1b726c6832d13c7b3c1d415c9af9b6e26
pattanaikay <pratikpattanaik821@gmail.com>
2025-11-25 08:50:02 +0530

    First Attempt at solving minDaysforBouquets, 1482 (Python, Binary Search)

---
+++ Binary Search/minDaysforBouquets.py
@@ -0,0 +1,21 @@
+def minDays(bloomDay: List[int], m: int, k: int) -> int:
+    def feasible(days)->bool:
+        bouquets, flowers = 0, 0
+        for bloom in bloomDay:
+            if bloom > days:
+                flowers = 0
+            else:
+                bouquets = (flowers + 1) // k
+                flowers = (flowers + 1) % k
+        return bouquets >= m
+    
+    if len(bloomDay) < m * k:
+        return -1
+    l, r = 1, max(bloomDay)
+    while l < r:
+        mid = l + (r-l) // 2
+        if feasible(mid):
+            r = mid
+        else:
+            l = mid + 1
+    return l
